b39c3e41a88745f4e085551306999ee0c763c49d
IsuruRanapana <113498635+IsuruRanapana@users.noreply.github.com>
2022-11-09 16:52:30 +0530

    Created bloc state and events using freezed

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:learning_project/domain/auth/auth_failures.dart';
+import 'package:learning_project/domain/auth/i_auth_facade.dart';
+import 'package:learning_project/domain/auth/value_objects.dart';
+import 'package:meta/meta.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial()) {
+    on<SignInFormEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
